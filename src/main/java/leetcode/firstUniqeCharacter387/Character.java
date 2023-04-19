9a7bd140bb4a63aa0467ff7a9295a2863bdcb36b
Your Name <you@example.com>
2023-04-19 12:18:32 +0200

    Create code in Character and CharacterTest.

--- src/main/java/leetcode/firstUniqeCharacter387/Character.java
+++ src/main/java/leetcode/firstUniqeCharacter387/Character.java
@@ -1,5 +1,19 @@
 package leetcode.firstUniqeCharacter387;
 
 public class Character {
-    
+
+
+    public int firstUniqChar(String s) {
+        int[] alphabet = new int[26];
+        for (int i = 0; i < s.length(); i++)
+            alphabet[s.charAt(i) - 'a']++;
+        for (int i = 0; i < s.length(); i++) {
+            if (alphabet[s.charAt(i) - 'a'] == 1) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+
 }
--- src/test/java/leetcode/firstUniqeCharacter387/CharacterTest.java
+++ src/test/java/leetcode/firstUniqeCharacter387/CharacterTest.java
@@ -1,5 +1,6 @@
 package leetcode.firstUniqeCharacter387;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -7,7 +8,19 @@ import static org.junit.jupiter.api.Assertions.*;
 class CharacterTest {
     @Test
     public void shouldReturnFirstUniqChar() {
+        //given
         Character character = new Character();
 
+        //when
+        String s = "leetcode";
+        String s2 = "loveleetcode";
+        String s3 = "aabb";
+        int output = 0;
+        int output2 = 2;
+        int output3 = -1;
+        //then
+        Assertions.assertEquals(output, character.firstUniqChar(s));
+        Assertions.assertEquals(output2, character.firstUniqChar(s2));
+        Assertions.assertEquals(output3, character.firstUniqChar(s3));
     }
 }
