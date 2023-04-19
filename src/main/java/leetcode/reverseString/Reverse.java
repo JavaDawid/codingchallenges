3bf5963aaf07609365b1d302e235123e402fde0e
Your Name <you@example.com>
2023-04-19 15:40:54 +0200

    Create class Reverse and ReverseTest with code.

---
+++ src/main/java/leetcode/reverseString/Reverse.java
@@ -0,0 +1,16 @@
+package leetcode.reverseString;
+
+public class Reverse {
+
+    public void reverseString(char[] s) {
+        int i = 0;
+        int j = s.length - 1;
+        while (i < j) {
+            char temporary = s[i];
+            s[i] = s[j];
+            s[j] = temporary;
+            i++;
+            j--;
+        }
+    }
+}
