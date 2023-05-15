2667cf10c7522e9d97f4d14b5f5307f7f20010c1
Your Name <you@example.com>
2023-05-15 13:35:43 +0200

    Character extract create alphabet to single method. CharacterTest modification //given //when //then.

--- src/main/java/leetcode/firstUniqeCharacter387/Character.java
+++ src/main/java/leetcode/firstUniqeCharacter387/Character.java
@@ -4,9 +4,7 @@ public class Character {
 
 
     public int firstUniqChar(String s) {
-        int[] alphabet = new int[26];
-        for (int i = 0; i < s.length(); i++)
-            alphabet[s.charAt(i) - 'a']++;
+        int[] alphabet = createAlphabet(s);
         for (int i = 0; i < s.length(); i++) {
             if (alphabet[s.charAt(i) - 'a'] == 1) {
                 return i;
@@ -15,5 +13,11 @@ public class Character {
         return -1;
     }
 
-
+    private static int[] createAlphabet(String s) {
+        int[] alphabet = new int[26];
+        for (int i = 0; i < s.length(); i++) {
+            alphabet[s.charAt(i) - 'a']++;
+        }
+        return alphabet;
+    }
 }
