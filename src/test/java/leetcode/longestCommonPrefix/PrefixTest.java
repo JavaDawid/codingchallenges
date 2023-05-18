3a9c07009217d2dedb3248c0e193d6aacf55457d
Your Name <you@example.com>
2023-05-18 20:16:44 +0200

    Add //when,//given,//then.

--- src/test/java/leetcode/longestCommonPrefix/PrefixTest.java
+++ src/test/java/leetcode/longestCommonPrefix/PrefixTest.java
@@ -8,6 +8,7 @@ import static org.junit.jupiter.api.Assertions.*;
 class PrefixTest {
     @Test
     public void shouldReturnPrefix() {
+        //given
         String[] array1 = {"flower", "flow", "flight"};
         String[] array2 = {"safower", "grow", "flight"};
         String[] array3 = {"power", "pow", "flight"};
@@ -17,9 +18,15 @@ class PrefixTest {
         String result3 = "";
         String result4 = "";
         Prefix prefix = new Prefix();
-        Assertions.assertEquals(result1, prefix.longestCommonPrefix(array1));
-        Assertions.assertEquals(result2, prefix.longestCommonPrefix(array2));
-        Assertions.assertEquals(result3, prefix.longestCommonPrefix(array3));
-        Assertions.assertEquals(result4, prefix.longestCommonPrefix(array4));
+        //when
+        String s = prefix.longestCommonPrefix(array1);
+        String s2 = prefix.longestCommonPrefix(array2);
+        String s3 = prefix.longestCommonPrefix(array3);
+        String s4 = prefix.longestCommonPrefix(array4);
+        //then
+        Assertions.assertEquals(result1, s);
+        Assertions.assertEquals(result2, s2);
+        Assertions.assertEquals(result3, s3);
+        Assertions.assertEquals(result4, s4);
     }
 }
